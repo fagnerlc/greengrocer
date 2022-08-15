953e4c0ee1dd92c68986b4b5d4d93decc1b2c814
fagnerlc <fagnerlc@outlook.com>
2022-08-15 00:28:21 -0300

    Data matching com freezed

---
+++ lib/src/pages/auth/repository/auth_errors.dart
@@ -0,0 +1,9 @@
+String authErrorsString(String? code) {
+  switch (code) {
+    case 'INVALID_CREDENTIALS':
+      return 'Email e/ou senha invalidos';
+    default:
+      return 'Um erro indefinido ocorreu';
+  }
+  return '';
+}
--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -1,10 +1,12 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/models/user_model.dart';
+import 'package:greengrocer/src/pages/auth/repository/auth_errors.dart' as authErrors;
+import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
-  Future signIn({
+  Future<AuthResult> signIn({
     required String email,
     required String password,
   }) async {
@@ -18,20 +20,22 @@ class AuthRepository {
     );
 
     if (result['result'] != null) {
-      print('Signin funcionou');
       final user = UserModel.fromJson(result['result']);
+      print('Signin funcionou');
       print('result[message]: ${result['message']}');
       print('result[error]: ${result['error']}');
       print('result[code]: ${result['code']}');
       print('result[result]: ${result['result']}');
       print('result: $result');
       print('user: $user');
+      return AuthResult.success(user);
     } else {
       print('Signin NÃO funcionou');
       print(result['message']);
       print(result['error']);
       print(result['code']);
       print(result);
+      return AuthResult.error(authErrors.authErrorsString(result['error']));
     }
   }
 }
