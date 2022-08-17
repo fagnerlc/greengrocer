547cb9e97e5153e76560f5034523322f2180bac0
fagnerlc <fagnerlc@outlook.com>
2022-08-17 19:19:14 -0300

    Criando Freezed com generics

--- lib/src/pages/home/view/home_tab.dart
+++ lib/src/pages/home/view/home_tab.dart
@@ -6,10 +6,10 @@ import 'package:get/get.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/pages/common_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages/common_widgets/custom_shimmer.dart';
-import 'package:greengrocer/src/pages/home/components/category_tile.dart';
 import 'package:greengrocer/src/config/app_data.dart' as app_data;
-import 'package:greengrocer/src/pages/home/components/item_tile.dart';
 import 'package:greengrocer/src/pages/home/controller/home_controller.dart';
+import 'package:greengrocer/src/pages/home/view/components/category_tile.dart';
+import 'package:greengrocer/src/pages/home/view/components/item_tile.dart';
 
 class HomeTab extends StatelessWidget {
   HomeTab({Key? key}) : super(key: key);
